54622cb971dd4e263729a814cd21b332a4d6a6e4
Mariana <maryazevedo2036@gmail.com>
2026-08-06 20:25:55 -0400

    feito

--- Aula/numerosPares.js
+++ Aula/numerosPares.js
@@ -1,10 +1,10 @@
 const prompt = require("prompt-sync")();
 
 let pairs = 0;
-for(let i = 1; i<= 10; i++){
-let n = Number(prompt("Number:"));
-if (n%2 === 0){
-pairs = pairs +1;
-}
+for (let i = 1; i <= 10; i++) {
+  n = 4;
+  if (n % 2 === 0) {
+    pairs = pairs + 1;
+  }
 }
 console.log("Number of ever numbers:", pairs);
